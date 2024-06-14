3f789742ba79bfd6afde26e64b833485fce810af
mjonker13@st.noorderpoort.nl <jonkermarvin1@gmail.com>
2024-06-14 15:24:00 +0200

    playercontroller update

--- RunAndGunGame/Assets/Scripts/Player/PlayerController.cs
+++ RunAndGunGame/Assets/Scripts/Player/PlayerController.cs
@@ -5,38 +5,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int movementSpeed;
-    public int jumpForce;
-    public int health;
+    public float movementSpeed;
+    public float jumpForce;
+    public float health;
 
     float horizontalMovement;
 
-    public bool canMove = true;
+    public GameObject Player;
+    public Transform Camera;
+    public Rigidbody2D playerRigidbody;
 
-    GameObject player;
+    public bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
         canMove = true;
+        movementSpeed *= Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        Camera.position = new Vector3(Player.transform.position.x, Camera.position.y, -10);
+
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        if (horizontalMovement > 0)
+        {
+            gameObject.transform.Translate(movementSpeed, 0, 0);
+            Player.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (horizontalMovement < 0)
+        {
+            gameObject.transform.Translate(-movementSpeed, 0, 0);
+            Player.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        Jump();
     }
 
-    public void Movement()
+    private void Jump()
     {
-        while (canMove)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            horizontalMovement = Input.GetAxisRaw("Horizontal");
-            if (horizontalMovement > 0)
-            {
-                print("test");
-                player.transform.Translate(1, 0, 0);
-            }
+            playerRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
+
+    
 }
